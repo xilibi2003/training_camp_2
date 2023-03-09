05e0c16fc9cb723654d2a9bbb4fc1478dd6bc5e3
xilibi2003@163.com <xilibi2003@163.com>
2023-03-09 15:57:45 +0800

    add config

--- w1_2/hardhat.config.js
+++ w1_2/hardhat.config.js
@@ -1,6 +1,31 @@
 require("@nomicfoundation/hardhat-toolbox");
 
+
+let dotenv = require('dotenv')
+dotenv.config({ path: "./.env" })
+
+const mnemonic = process.env.MNEMONIC
+const privateKey = process.env.PRIVATEKEY
+
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.18",
+
+  networks: {
+    hardhat: {
+      chainId: 31337,
+      gas: 12000000,
+      accounts: {
+        mnemonic: mnemonic,
+      },
+    },
+
+    mumbai: {
+      url: "https://endpoints.omniatech.io/v1/matic/mumbai/public",
+      accounts: [privateKey],
+      chainId: 80001,
+    },
+
+  }
+
 };
