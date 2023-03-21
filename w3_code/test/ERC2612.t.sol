1ec6e54cf2e890aa8473cd53e806d2682c968b40
xilibi2003@163.com <xilibi2003@163.com>
2023-03-21 18:41:10 +0800

    add permit test

---
+++ w3_code/test/ERC2612.t.sol
@@ -0,0 +1,57 @@
+pragma solidity ^0.8.13;
+
+import "forge-std/Test.sol";
+import "../src/ERC2612.sol";
+import "../src/SigUtils.sol";
+
+contract ERC20Test is Test {
+    ERC2612 internal token;
+    SigUtils internal sigUtils;
+
+    uint256 internal ownerPrivateKey;
+    uint256 internal spenderPrivateKey;
+
+    address internal owner;
+    address internal spender;
+
+    function setUp() public {
+        token = new ERC2612();
+        sigUtils = new SigUtils(token.DOMAIN_SEPARATOR());
+
+        ownerPrivateKey = 0xA11CE;
+        spenderPrivateKey = 0xB0B;
+
+        owner = vm.addr(ownerPrivateKey);
+        spender = vm.addr(spenderPrivateKey);
+
+        token.transfer(owner, 1e18);
+    }
+
+    function test_Permit() public {
+        SigUtils.Permit memory permit = SigUtils.Permit({
+            owner: owner,
+            spender: spender,
+            value: 1e18,
+            nonce: 0,
+            deadline: 1 days
+        });
+
+        bytes32 digest = sigUtils.getTypedDataHash(permit);
+
+        (uint8 v, bytes32 r, bytes32 s) = vm.sign(ownerPrivateKey, digest);
+
+        token.permit(
+            permit.owner,
+            permit.spender,
+            permit.value,
+            permit.deadline,
+            v,
+            r,
+            s
+        );
+
+        assertEq(token.allowance(owner, spender), 1e18);
+        assertEq(token.nonces(owner), 1);
+    }
+
+}
