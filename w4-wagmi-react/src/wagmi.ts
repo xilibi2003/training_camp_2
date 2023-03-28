a1c01f267f75d88d19b7ff20176060569dc98498
xilibi2003@163.com <xilibi2003@163.com>
2023-03-28 12:49:12 +0800

    add w4-wagmi

---
+++ w4-wagmi-react/src/wagmi.ts
@@ -0,0 +1,42 @@
+import { configureChains, createClient } from 'wagmi'
+import { goerli, mainnet } from 'wagmi/chains'
+import { CoinbaseWalletConnector } from 'wagmi/connectors/coinbaseWallet'
+import { InjectedConnector } from 'wagmi/connectors/injected'
+import { MetaMaskConnector } from 'wagmi/connectors/metaMask'
+import { WalletConnectLegacyConnector } from 'wagmi/connectors/walletConnectLegacy'
+import { publicProvider } from 'wagmi/providers/public'
+
+const { chains, provider, webSocketProvider } = configureChains(
+  [mainnet, ...(import.meta.env?.MODE === 'development' ? [goerli] : [])],
+  [
+    publicProvider(),
+  ],
+)
+
+export const client = createClient({
+  autoConnect: true,
+  connectors: [
+    new MetaMaskConnector({ chains }),
+    new CoinbaseWalletConnector({
+      chains,
+      options: {
+        appName: 'wagmi',
+      },
+    }),
+    new WalletConnectLegacyConnector({
+      chains,
+      options: {
+        qrcode: true,
+      },
+    }),
+    new InjectedConnector({
+      chains,
+      options: {
+        name: 'Injected',
+        shimDisconnect: true,
+      },
+    }),
+  ],
+  provider,
+  webSocketProvider,
+})
